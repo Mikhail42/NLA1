b8f40724c75411a34f69d238235fa19b043de108
Михаил Ионкин <ionkin-rus@mail.ru>
2015-09-25 00:25:24 +0400

    Update MyDouble.scala

--- src/LR1/MyDouble.scala
+++ src/LR1/MyDouble.scala
@@ -6,7 +6,7 @@ object MyDouble {
   import math._
   val ERROR = 1e-8
   def compareDouble(a: Double, b: Double) = if (abs(a-b)<ERROR) 0 else if (a>b) 1 else -1
-  // !!! else program input error. sorry for my bad english
+  // !!! If you do otherwise, the program will work properly!
   def sign(x: Double) = if (compareDouble(x,0.0)>=0) 1 else -1
   def isZero(x: Double) = abs(x)<ERROR
 }

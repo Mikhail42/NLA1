1910ed430c587302fd1a782ed633d0275b8457c6
Михаил Ионкин <ionkin-rus@mail.ru>
2015-09-25 00:23:16 +0400

    Update Counter.scala

--- src/LR1/Counter.scala
+++ src/LR1/Counter.scala
@@ -1,8 +1,18 @@
 package LR1
+
 /**
  * @author misha
+ * This object (!!!) is used in the calculation of chislel operations from the set {*, /, +, - =,?}.
  */
 object Counter {
-    // count operations both multi and delete
-    var countOpsMD = 0
+    /**  multi and delete */
+    var countMD: Long = 0
+    /**  plus, minus */
+    var countPM: Long = 0
+    /** Initialization numbers or assigning them values  */
+    var countIA: Long = 0
+    /** comparison  */
+    var countC: Long = 0
+    def setZeros(){countMD = 0; countPM = 0; countIA = 0; countC = 0;}
+    override def toString() = (" {*,/} - "+countMD+"; {+,-} - "+countPM +"; {=} - "+countIA+"; {>,<,==} - "+countC)
 }
